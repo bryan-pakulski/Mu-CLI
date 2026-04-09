33be0819911a26d32da1ce77a85dca27c32f952b
bryan pakulski <BryanPakulski@outlook.com>
2026-04-09 11:03:32 +1000

    fix missing div tag import in Van session list component

--- gui/van/components/sessions/session_list.js
+++ gui/van/components/sessions/session_list.js
@@ -1,6 +1,6 @@
 import van from "../../../vendor/van-1.6.0.min.js";
 
-const { button, h2, li, section, ul } = van.tags;
+const { button, div, h2, li, section, ul } = van.tags;
 
 function renderSessionItems(store, api) {
   const fragment = document.createDocumentFragment();
